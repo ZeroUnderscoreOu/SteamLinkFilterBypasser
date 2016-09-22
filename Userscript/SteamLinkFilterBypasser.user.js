7fe338a6befad61f7de83c05cd152a03549f32d4
ZeroUnderscoreOu <ZeroUnderscoreOu@Yandex.ru>
2016-09-22 11:58:02 +0300

    1.1.0 - Added support for anchors in URLs; - updated metadata.

--- Userscript/SteamLinkFilterBypasser.user.js
+++ Userscript/SteamLinkFilterBypasser.user.js
@@ -1,14 +1,12 @@
 // ==UserScript==
 // @name        Steam Link Filter Bypasser
 // @author      ZeroUnderscoreOu
-// @version     1.0.1
+// @version     1.1.0
 // @icon        https://raw.githubusercontent.com/ZeroUnderscoreOu/SteamLinkFilterBypasser/master/Logo128.png
-// @downloadURL https://raw.githubusercontent.com/ZeroUnderscoreOu/SteamLinkFilterBypasser/master/Greasemonkey/SteamLinkFilterBypasser.user.js
-// @updateURL   https://raw.githubusercontent.com/ZeroUnderscoreOu/SteamLinkFilterBypasser/master/Greasemonkey/SteamLinkFilterBypasser.user.js
 // @namespace   https://github.com/ZeroUnderscoreOu/
 // @include     *://steamcommunity.com/linkfilter/?url=*
 // @run-at      document-start
 // @grant       none
 // ==/UserScript==
 
-document.location.href = document.location.search.match(/\?url=(.*)/)[1];
+document.location.href = document.location.search.match(/\?url=(.*)/)[1] + document.location.hash; // preserving possible anchors
